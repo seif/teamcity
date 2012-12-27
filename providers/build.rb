125633db4ccd62a43f3946feae00ba358b5d75b1
Seif Attar <iam@seifattar.net>
2012-12-27 17:29:31 +0000

    Workaround for LWRP DSL bug
    Bug was preventing the teamcity_build resource
    from firing notifies
    http://tickets.opscode.com/browse/CHEF-3681
    Workaround based of
    https://gist.github.com/d85be145f3ff824ccc07/0e97d44f20ec65411b141cb46cbeb19bb34b44ad

--- providers/build.rb
+++ providers/build.rb
@@ -20,6 +20,37 @@
 include Teamcity::Helper
 
 action :download do
-  initialize_connection(@new_resource.connection)
-  download_all(@new_resource.destination)
+  # So that we can refer to these within the sub-run-context block.
+  cached_new_resource = new_resource
+  cached_current_resource = current_resource
+
+  # Setup a sub-run-context.
+  sub_run_context = @run_context.dup
+  sub_run_context.resource_collection = Chef::ResourceCollection.new
+
+  # Declare sub-resources within the sub-run-context. Since they are declared here,
+  # they do not pollute the parent run-context.
+  begin
+    original_run_context, @run_context = @run_context, sub_run_context
+    
+    # Do the actual work for this action:
+    initialize_connection(@new_resource.connection)
+    download_all(@new_resource.destination)
+  rescue => e
+    # For some reason errors were not bubbling up unless caught and raised
+    Chef::Log.fatal(e)
+    raise
+  ensure
+    @run_context = original_run_context
+  end
+
+  # Converge the sub-run-context inside the provider action.
+  # Make sure to mark the resource as updated-by-last-action if any sub-run-context
+  # resources were updated (any actual actions taken against the system) during the
+  # sub-run-context convergence.
+  begin
+    Chef::Runner.new(sub_run_context).converge
+  ensure
+    new_resource.updated_by_last_action(true)
+  end
 end
